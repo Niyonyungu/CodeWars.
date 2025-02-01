aab9170b106ec228b799dd8818b9fe1c19428ac3
vainqueur <vainqueurmg@gmail.com>
2025-02-01 22:57:19 +0200

    practice

--- practice.js
+++ practice.js
@@ -2,5 +2,21 @@
 
 
 
+const players = [
+    { name: 'messi', goals: 30 },
+    { name: 'ronaldo', goals: 25 },
+    { name: 'neymar', goals: 15 },
+    { name: 'mbappe', goals: 28 },
+    { name: 'haaland', goals: 35 },
+]
+
+const results = players
+    .filter(player => player.goals > 20)
+    .map(player => player.name)
+    .sort()
+    .reverse()
+
+console.log(results)
+
 
 
